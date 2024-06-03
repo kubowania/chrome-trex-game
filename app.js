aa2a1774fd69770ec138afcc4a58a3461058f4d4
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2024-06-03 20:49:51 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
-const dino = document.querySelector('.dino')
-const grid = document.querySelector('.grid')
-const body = document.querySelector('body')
-const alert = document.getElementById('alert')
-let isJumping = false
-let gravity = 0.9
-let isGameOver = false
+document.addEventListener('DOMContentLoaded', function() {
+    const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    const alert = document.getElementById('alert')
+    let gravity = 0.9
+    let isJumping = false
+    let isGameOver = false
 
-function control(e) {
-  if (e.keyCode === 32) {
-    if (!isJumping) {
-      isJumping = true
-      jump()
-    }
-  }
-}
-document.addEventListener('keyup', control)
-
-let position = 0
-function jump() {
-  let count = 0
-  let timerId = setInterval(function () {
-    //move down
-    if (count === 15) {
-      clearInterval(timerId)
-      let downTimerId = setInterval(function () {
-        if (count === 0) {
-          clearInterval(downTimerId)
-          isJumping = false
+    function control(e) {
+        if (e.code === "Space") {
+            if (!isJumping) {
+                jump()
+            }
         }
-        position -= 5
-        count--
-        position = position * gravity
-        dino.style.bottom = position + 'px'
-      },20)
-
     }
-    //move up
-    position +=30
-    count++
-    position = position * gravity
-    dino.style.bottom = position + 'px'
-  },20)
-}
+    document.addEventListener('keydown', control)
 
-function generateObstacles() {
-  let randomTime = Math.random() * 4000
-  let obstaclePosition = 1000
-  const obstacle = document.createElement('div')
-  if (!isGameOver) obstacle.classList.add('obstacle')
-  grid.appendChild(obstacle)
-  obstacle.style.left = obstaclePosition + 'px'
-
-  let timerId = setInterval(function() {
-    if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
-      clearInterval(timerId)
-      alert.innerHTML = 'Game Over'
-      isGameOver = true
-      //remove all children
-      body.removeChild(body.firstChild)
-      while (grid.firstChild) {
-        grid.removeChild(grid.lastChild)
-      }
-      
+    let position = 0
+    function jump() {
+        isJumping = true
+        let count = 0
+        let timerId = setInterval(function() {
+            //move down
+            if (count === 15) {
+                clearInterval(timerId)
+                let downTimerId = setInterval(function() {
+                    if (count === 0) {
+                        clearInterval(downTimerId)
+                        isJumping = false
+                    }
+                    position -=5
+                    count--
+                    position = position * gravity
+                    dino.style.bottom = position + 'px'
+                }, 20)
+            }
+            //move up
+            position += 30
+            count++
+            position = position * gravity
+            dino.style.bottom = position + 'px'
+        }, 20)
     }
-    obstaclePosition -=10
-    obstacle.style.left = obstaclePosition + 'px'
-  },20)
-  if (!isGameOver) setTimeout(generateObstacles, randomTime)
-}
-generateObstacles()
+
+    function generateObstacles() {
+        if (!isGameOver) {
+            let randomTime = Math.random() * 4000
+            let obstaclePosition = 1000
+            const obstacle = document.createElement('div')
+            obstacle.classList.add('obstacle')
+            grid.appendChild(obstacle)
+            obstacle.style.left = obstaclePosition + 'px'
+
+            let timerId = setInterval(function() {
+                if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+                    clearInterval(timerId)
+                    alert.innerHTML = 'Game Over'
+                    isGameOver = true
+                    //remove all children
+                    while (grid.firstChild) {
+                        grid.removeChild(grid.lastChild)
+                    }
+                }
+                obstaclePosition -=10
+                obstacle.style.left = obstaclePosition + 'px'
+            }, 20)
+            setTimeout(generateObstacles, randomTime)
+        }
+    }
+    generateObstacles()
 })
